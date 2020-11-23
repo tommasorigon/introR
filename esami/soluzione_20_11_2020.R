cd65a90f3e32274008271bd2be89314ea3fd1b86
Tommaso Rigon <tommaso.rigon@gmail.com>
2020-11-23 11:21:04 +0100

    asd

--- esami/soluzione_20_11_2020.R
+++ esami/soluzione_20_11_2020.R
@@ -19,22 +19,22 @@ boxplot.stats(forbes$pres)
 # In alternativa si poteva fare un grafico tramite il comando boxplot
 
 # Punto 6.3
-par(mfrow=c(1,1))
-hist(forbes$bp, breaks=5)
-hist(forbes$pres, breaks=5)
+par(mfrow = c(1, 1))
+hist(forbes$bp, breaks = 5)
+hist(forbes$pres, breaks = 5)
 
 # Punto 6.4
-par(mfrow=c(1,1))
-plot(forbes$bp,forbes$pres) # Ulteriori abbellimenti grafici erano apprezzati ma non necessari
+par(mfrow = c(1, 1))
+plot(forbes$bp, forbes$pres) # Ulteriori abbellimenti grafici erano apprezzati ma non necessari
 
 # Punto 6.5
 cor(forbes$bp, forbes$pres)
 
 # Punto 6.6
-ols <- function(x,y){
-  beta_hat <- cov(x,y)/var(x)
-  alpha_hat <- mean(y) - mean(x)*beta_hat
-  c(alpha_hat,beta_hat)  
+ols <- function(x, y) {
+  beta_hat <- cov(x, y) / var(x)
+  alpha_hat <- mean(y) - mean(x) * beta_hat
+  c(alpha_hat, beta_hat)
 }
 ols_est <- ols(forbes$bp, forbes$pres)
 ols_est
@@ -50,26 +50,26 @@ abline(ols_est) # Questo è il comando più semplice in assoluto, ma esistono va
 
 
 # Punto 7.1
-dburr <- function(x, alpha, beta){
-  alpha*beta*x^(alpha-1)/(1 + x^alpha)^(beta+1)
+dburr <- function(x, alpha, beta) {
+  alpha * beta * x^(alpha - 1) / (1 + x^alpha)^(beta + 1)
 }
 
-pburr <- function(x, alpha, beta){
-  1 - 1/(1 + x^alpha)^beta
+pburr <- function(x, alpha, beta) {
+  1 - 1 / (1 + x^alpha)^beta
 }
 
-qburr <- function(p, alpha, beta){
-  (1/(1 - p)^1/beta - 1)^(1/alpha)
+qburr <- function(p, alpha, beta) {
+  (1 / (1 - p)^1 / beta - 1)^(1 / alpha)
 }
 
 # Controllo che sia tutto corretto: qburr(pburr(10,2,1),2,1)
 
 # Punto 7.2
-curve(dburr(x,2,1),0,5)
+curve(dburr(x, 2, 1), 0, 5)
 
 # Punto 7.3
-rburr <- function(R, alpha, beta){
-  qburr(runif(R),alpha,beta)
+rburr <- function(R, alpha, beta) {
+  qburr(runif(R), alpha, beta)
 }
 
 # Punto 7.4
@@ -80,9 +80,9 @@ mean(x > 2 & x < 3)
 # Ultimo comando equivalente a: pburr(3,2,1) - pburr(2,2,1)
 
 # Punto 7.5
-hist(x, breaks=1000, freq=FALSE,xlim=c(0,5)) # Ci sono dei valori anomali, è necessario restringere il range
-curve(dburr(x, 2, 1), add=TRUE)
+hist(x, breaks = 1000, freq = FALSE, xlim = c(0, 5)) # Ci sono dei valori anomali, è necessario restringere il range
+curve(dburr(x, 2, 1), add = TRUE)
 
 # Punto 7.6
-plot(ecdf(x),xlim=c(0,5)) # Ci sono dei valori anomali, è necessario restringere il range
-curve(pburr(x,2,1),add=TRUE, col="red")
+plot(ecdf(x), xlim = c(0, 5)) # Ci sono dei valori anomali, è necessario restringere il range
+curve(pburr(x, 2, 1), add = TRUE, col = "red")
