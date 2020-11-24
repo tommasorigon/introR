ec1ab44d57adbd3e9a7211006533b932f4d0ec0f
Tommaso Rigon <tommaso.rigon@gmail.com>
2020-11-24 16:10:28 +0100

    asd

--- exe/es_1.R
+++ exe/es_1.R
@@ -78,10 +78,10 @@ sum((-1)^(1:n) * (1:n) * choose(n, 1:n)) # ERRORE NUMERICO!
 
 # La soluzione di un'equazione di secondo grado è ovviamente disponibile in forma chiusa
 
-roots <- function(a,b,c){
-  Delta <- b^2 - 4*a*c
-  if(Delta < 0) stop("Il delta deve essere non negativo")
-  (- b + c(-1, 1)*sqrt(Delta)) / (2 * a)
+roots <- function(a, b, c) {
+  Delta <- b^2 - 4 * a * c
+  if (Delta < 0) stop("Il delta deve essere non negativo")
+  (-b + c(-1, 1) * sqrt(Delta)) / (2 * a)
 }
 
 # D.1
@@ -95,3 +95,34 @@ roots(1, 2, 1)
 # D.3
 
 roots(1, 1, 1) # Non esistono soluzioni reali
+
+# Esercizio E -------------------------------
+
+A <- rbind(
+  c(26, 22, 17, 22, 23),
+  c(22, 18, 14, 23, 27),
+  c(17, 14, 14, 20, 24),
+  c(22, 23, 20, 26, 23),
+  c(23, 27, 24, 23, 12)
+)
+
+# E.1
+
+det(solve(A)) # 0.0006493506
+1/det(A) # Stesso valore
+
+# E.2
+
+det(A%*%A) # 2371600
+det(A)^2 # Stesso valore
+
+# E.3
+
+sum(diag(A))
+
+# E.4
+
+det(A)
+prod(eigen(A)$values)
+
+# Esercizio F -----------------------
