146d42373d64ec08f98761d28c340e5cbf1a1f53
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-08-05 18:17:41 +0200

    Updates

--- lezioni/un_E.R
+++ lezioni/un_E.R
@@ -23,3 +23,51 @@ my_var <- function(x) {
 
 my_var(emo_A)
 my_var(emo_B)
+
+var(emo_A)
+var(emo_B)
+
+my_sd <- function(x) {
+  sqrt(my_var(x))
+}
+
+my_sd(emo_A)
+my_sd(emo_B)
+
+sd(emo_A)
+sd(emo_B)
+
+max(emo_A) - min(emo_A)
+max(emo_B) - min(emo_B)
+
+diff(range(emo_A))
+diff(range(emo_B))
+
+interquartile_range <- function(x){
+  diff(quantile(x, probs = c(0.25, 0.75)))
+}
+
+interquartile_range(emo_A)
+interquartile_range(emo_B)
+
+MAD <- function(x) {
+  median(abs(x - median(x)))
+}
+
+MAD(emo_A)
+MAD(emo_B)
+
+tapply(emoglobina$emoglobina, emoglobina$Metodologia, mean)
+
+with(emoglobina, tapply(emoglobina, Metodologia, list(mean, sd)))
+
+tab <- rbind(with(emoglobina, tapply(emoglobina, Metodologia, my_var)),
+             with(emoglobina, tapply(emoglobina, Metodologia, my_sd)),
+             with(emoglobina, tapply(emoglobina, Metodologia, interquartile_range)),
+             with(emoglobina, tapply(emoglobina, Metodologia, MAD)))
+rownames(tab) <- c("Varianza", "Deviazione Standard", "Scarto interquartile", "MAD")
+tab
+
+with(emoglobina, tapply(emoglobina, Metodologia, function(x) c(my_var(x), my_sd(x), interquartile_range(x), MAD(x)))
+
+     
---
+++ lezioni/un_F.R
@@ -0,0 +1,32 @@
+# Per $n = `r nrow(forbes)`$ luoghi nelle Alpi viene misurata la pressione atmosferica (inHg) e la temperatura di ebollizione dell'acqua (in gradi Celsius). I dati provengono da un esperimento condotto da Forbes nel 1857. I dati sono riportati nella seguente tabella.
+# 
+# ```{r, results="asis", echo=F}
+# kable(forbes)
+# ```
+# 
+# 1. Si disegni un istogramma della variabile `temperatura`, scegliendo un numero appropriato di classi equispaziate e giustificandone la scelta.
+# 
+# 1. Si ottengano la media aritmetica di entrambe le variabili `temperatura` e `pressione`. La relazione tra gradi Celsius e gradi Fahrenheit è la seguente:
+# $$
+# (\text{Fahrenheit}) = 32 + \frac{9}{5}(\text{Celsius}).
+# $$
+# Quanto vale la temperatura di ebollizione media espressa in gradi Fahrenheit? Si risponda senza ricalcolare tutti i valori della variabile `temperatura`.
+# 
+# 1. Si ottenga la varianza delle variabili `temperatura` e `pressione`.
+# 
+# 1. Si disegni un opportuno grafico che aiuti a comprendere la relazione tra le due variabili. Si calcoli quindi la correlazione: il valore ottenuto è coerente col grafico?
+# 
+# 1. Si ottenga la retta ai minimi quadrati per la relazione tra `temperatura` (variabile esplicativa $x$) e `pressione` (variabile risposta $y$), e la si disegni nel grafico ottenuto in precedenza. Si fornisca quindi un'interpretazione per i coefficienti.
+# 
+# 1. In base al modello stimato, se la temperatura di ebollizione dell'acqua è pari 97 gradi Celsius, a quanto è pari la pressione?
+# 
+# 1. Si ottenga un indice di bontà di adattamento ai dati della curva ottenuta e lo si interpreti nel contesto del problema.
+
+
+library(MASS)
+data("forbes")
+colnames(forbes)  <- c("Temperatura","Pressione")
+forbes$Temperatura <- (forbes$Temperatura - 32)*5/9
+forbes <- round(forbes,2)
+y <- forbes$Pressione
+x <- forbes$Temperatura
