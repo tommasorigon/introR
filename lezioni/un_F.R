945f96e956456aae3a15f91d8182998f89a03fb8
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-08-06 15:26:42 +0200

    Update un_F.R

--- lezioni/un_F.R
+++ lezioni/un_F.R
@@ -1,33 +1,57 @@
 # Per $n = `r nrow(forbes)`$ luoghi nelle Alpi viene misurata la pressione atmosferica (inHg) e la temperatura di ebollizione dell'acqua (in gradi Celsius). I dati provengono da un esperimento condotto da Forbes nel 1857. I dati sono riportati nella seguente tabella.
-# 
+#
 # ```{r, results="asis", echo=F}
 # kable(forbes)
 # ```
-# 
+#
 # 1. Si disegni un istogramma della variabile `temperatura`, scegliendo un numero appropriato di classi equispaziate e giustificandone la scelta.
-# 
+#
 # 1. Si ottengano la media aritmetica di entrambe le variabili `temperatura` e `pressione`. La relazione tra gradi Celsius e gradi Fahrenheit è la seguente:
 # $$
 # (\text{Fahrenheit}) = 32 + \frac{9}{5}(\text{Celsius}).
 # $$
 # Quanto vale la temperatura di ebollizione media espressa in gradi Fahrenheit? Si risponda senza ricalcolare tutti i valori della variabile `temperatura`.
-# 
+#
 # 1. Si ottenga la varianza delle variabili `temperatura` e `pressione`.
-# 
+#
 # 1. Si disegni un opportuno grafico che aiuti a comprendere la relazione tra le due variabili. Si calcoli quindi la correlazione: il valore ottenuto è coerente col grafico?
-# 
+#
 # 1. Si ottenga la retta ai minimi quadrati per la relazione tra `temperatura` (variabile esplicativa $x$) e `pressione` (variabile risposta $y$), e la si disegni nel grafico ottenuto in precedenza. Si fornisca quindi un'interpretazione per i coefficienti.
-# 
+#
 # 1. In base al modello stimato, se la temperatura di ebollizione dell'acqua è pari 97 gradi Celsius, a quanto è pari la pressione?
-# 
+#
 # 1. Si ottenga un indice di bontà di adattamento ai dati della curva ottenuta e lo si interpreti nel contesto del problema.
 
-rm(list=ls())
-forbes <- read.table("https://tommasorigon.github.io/introR/data/forbes2.csv", header = TRUE, sep = ",")
+rm(list = ls())
+forbes <- read.table("https://tommasorigon.github.io/introR/data/forbes.csv", header = TRUE, sep = ",")
 head(forbes)
 dim(forbes)
 colnames(forbes) <- c("TempF", "Pressione")
 
+# Creazione variabile in celsius
 forbes$TempC <- (forbes$TempF - 32) * 5 / 9 # Da Farenheit a Celsius
 
+par(mfrow = c(1, 2))
+hist(forbes$TempC, breaks = "sturges")
 
+breaks <- c(90, 92.5, 95, 97.5, 100, 102.5)
+hist(forbes$TempC, breaks = breaks)
+
+mean(forbes$TempC)
+mean(forbes$Pressione)
+
+mean(forbes$TempF)
+32 + 9 / 5 * mean(forbes$TempC)
+
+my_var <- function(x) mean(x^2) - mean(x)^2
+
+my_var(forbes$TempC)
+my_var(forbes$Pressione)
+
+par(mfrow=c(1, 1))
+plot(forbes$TempC, forbes$Pressione)
+plot(forbes$TempC, forbes$Pressione, pch = 16, xlab = "Temperatura", ylab = "Pressione")
+
+my_cov <- function(x, y) mean(x * y) - mean(x)*mean(y)
+
+my_cov
