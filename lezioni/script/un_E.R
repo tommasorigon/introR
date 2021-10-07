4631784cd3ea385edc3e84e868c0298b59dc36fd
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-10-07 17:30:15 +0200

    Update un_E.R

--- lezioni/script/un_E.R
+++ lezioni/script/un_E.R
@@ -1,5 +1,5 @@
 path <- "https://tommasorigon.github.io/introR/data/emoglobina.csv"
-emoglobina <- read.table(path, header = TRUE, sep = ",", stringsAsFactors = TRUE) # Scarica il file da internet
+emoglobina <- read.table(path, header = TRUE, sep = ",", stringsAsFactors = TRUE)
 
 str(emoglobina)
 
@@ -10,12 +10,15 @@ summary(emo_A)
 summary(emo_B)
 
 boxplot(emoglobina$emoglobina ~ emoglobina$Metodologia,
-        ylab = "Emoglobina",
-        xlab = "Metodologia"
+  ylab = "Emoglobina",
+  xlab = "Metodologia"
 )
 
-mean((emo_A - mean(emo_A))^2)
-mean((emo_B - mean(emo_B))^2)
+mean((emo_A - mean(emo_A))^2) # Varianza del gruppo A
+mean((emo_B - mean(emo_B))^2) # Varianza del gruppo B
+
+mean(emo_A^2) - mean(emo_A)^2 # Varianza del gruppo A, formula alternativa
+mean(emo_B^2) - mean(emo_B)^2 # Varianza del gruppo B, formula alternativa
 
 my_var <- function(x) {
   mean(x^2) - mean(x)^2
@@ -43,7 +46,7 @@ max(emo_B) - min(emo_B)
 diff(range(emo_A))
 diff(range(emo_B))
 
-interquartile_range <- function(x){
+interquartile_range <- function(x) {
   diff(quantile(x, probs = c(0.25, 0.75)))
 }
 
@@ -58,16 +61,26 @@ MAD(emo_A)
 MAD(emo_B)
 
 tapply(emoglobina$emoglobina, emoglobina$Metodologia, mean)
+with(emoglobina, tapply(emoglobina, Metodologia, mean)) # In maniera ancora più compatta
 
-with(emoglobina, tapply(emoglobina, Metodologia, list(mean, sd)))
-
-tab <- rbind(with(emoglobina, tapply(emoglobina, Metodologia, my_var)),
-             with(emoglobina, tapply(emoglobina, Metodologia, my_sd)),
-             with(emoglobina, tapply(emoglobina, Metodologia, interquartile_range)),
-             with(emoglobina, tapply(emoglobina, Metodologia, MAD)))
+tab <- rbind(
+  with(emoglobina, tapply(emoglobina, Metodologia, my_var)),
+  with(emoglobina, tapply(emoglobina, Metodologia, my_sd)),
+  with(emoglobina, tapply(emoglobina, Metodologia, interquartile_range)),
+  with(emoglobina, tapply(emoglobina, Metodologia, MAD))
+)
 rownames(tab) <- c("Varianza", "Deviazione Standard", "Scarto interquartile", "MAD")
 tab
 
-with(emoglobina, tapply(emoglobina, Metodologia, function(x) c(my_var(x), my_sd(x), interquartile_range(x), MAD(x)))
+# Prima possibile implementazione
+asym <- function(x) {
+  sqm <- sqrt(mean(x^2) - mean(x)^2)
+  mean((x - mean(x))^3) / sqm^3
+}
 
-     
+# Implementazione (leggermente) alternativa
+asym <- function(x) {
+  sqm <- sqrt(mean(x^2) - mean(x)^2)
+  z <- (x - mean(x)) / sqm
+  mean(z^3)
+}
