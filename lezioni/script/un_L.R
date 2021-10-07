db4c243c8581f8e67c011701476db2d59f9e53cf
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-10-07 18:09:11 +0200

    Update un_L.R

--- lezioni/script/un_L.R
+++ lezioni/script/un_L.R
@@ -2,32 +2,19 @@ rm(list = ls())
 ciliegi <- read.csv("https://tommasorigon.github.io/introR/data/ciliegi.csv", header = TRUE)
 head(ciliegi)
 
+plot(ciliegi)
 
+# Funzione di perdita che vogliamo minimizzare
 loss <- function(par, y, x) {
   mean((y - par[1] * x^par[2])^2)
 }
 
-loss(c(12, 1e-4), ciliegi$volume, ciliegi$diametro)
+loss(c(1, 1), ciliegi$volume, ciliegi$diametro)
 
-
-eta <- seq(0.015, 0.4, length = 200)
-lambda <- seq(1.5, 3, length = 200)
-parvalues <- expand.grid(eta, lambda)
-
-lossvalues <- apply(parvalues, 1, loss, y = ciliegi$volume, x = ciliegi$diametro)
-lossvalues <- matrix(lossvalues, nrow = length(eta), ncol = length(lambda), byrow = F)
-
-filled.contour(eta, lambda, lossvalues,
-               xlab = expression(eta), ylab = expression(lambda),
-               levels = seq(from = 0, to = 25, by = 2),
-               #col = terrain.colors(20),
-               main = "Funzione di perdita"
+fit_ls <- nlminb(
+  start = c(1, 1), function(param) loss(param, ciliegi$volume, ciliegi$diametro),
+  lower = c(1e-6, 1e-6)
 )
-
-
-
-fit_ls <- nlminb(start = c(1, 1), function(param) loss(param, ciliegi$volume, ciliegi$diametro),
-                 lower = c(1e-6, 1e-6))
 fit_ls
 
 param_hat_ls <- fit_ls$par
@@ -35,16 +22,15 @@ param_hat_ls <- fit_ls$par
 z <- log(ciliegi$volume)
 w <- log(ciliegi$diametro)
 
-beta_hat_ols <- cov(z, log(ciliegi$diametro)) / var(w)
+beta_hat_ols <- cov(w, z) / var(w)
 alpha_hat_ols <- mean(z) - mean(w) * beta_hat_ols
 
+# Stima ai minimi quadrati, scala trasformata
 param_hat_ols <- c(exp(alpha_hat_ols), beta_hat_ols)
-
-cbind(param_hat_ls, param_hat_ols)
-
-loss(param_hat_ls, ciliegi$volume, ciliegi$diametro)
+param_hat_ols
+# Varianza residuale
 loss(param_hat_ols, ciliegi$volume, ciliegi$diametro)
 
 plot(ciliegi)
-curve(param_hat_ls[1] * x^param_hat_ls[2], add = TRUE, lty = "dashed")
-curve(param_hat_ols[1] * x^param_hat_ols[2], add = TRUE, lty = "dashed", col = "red")
+curve(param_hat_ls[1] * x^param_hat_ls[2], add = TRUE, lty = "dashed") # Non-lineari
+curve(param_hat_ols[1] * x^param_hat_ols[2], add = TRUE, lty = "dashed", col = "red") # Modello linearizzato
