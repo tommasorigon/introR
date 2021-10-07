803f0d683f6632be7995a6abcc0c97e778df08ee
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-10-07 16:46:18 +0200

    Updates

--- lezioni/script/un_L.R
+++ lezioni/script/un_L.R
@@ -7,15 +7,36 @@ loss <- function(par, y, x) {
   mean((y - par[1] * x^par[2])^2)
 }
 
-loss(c(1, 1), ciliegi$volume, ciliegi$diametro)
+loss(c(12, 1e-4), ciliegi$volume, ciliegi$diametro)
 
-fit_ls <- nlminb(start = c(1, 1), function(param) loss(param, ciliegi$volume, ciliegi$diametro), lower = c(1e-6, 1e-6))
+
+eta <- seq(0.015, 0.4, length = 200)
+lambda <- seq(1.5, 3, length = 200)
+parvalues <- expand.grid(eta, lambda)
+
+lossvalues <- apply(parvalues, 1, loss, y = ciliegi$volume, x = ciliegi$diametro)
+lossvalues <- matrix(lossvalues, nrow = length(eta), ncol = length(lambda), byrow = F)
+
+filled.contour(eta, lambda, lossvalues,
+               xlab = expression(eta), ylab = expression(lambda),
+               levels = seq(from = 0, to = 25, by = 2),
+               #col = terrain.colors(20),
+               main = "Funzione di perdita"
+)
+
+
+
+fit_ls <- nlminb(start = c(1, 1), function(param) loss(param, ciliegi$volume, ciliegi$diametro),
+                 lower = c(1e-6, 1e-6))
 fit_ls
 
 param_hat_ls <- fit_ls$par
 
-beta_hat_ols <- cov(log(ciliegi$volume), log(ciliegi$diametro)) / var(log(ciliegi$diametro))
-alpha_hat_ols <- mean(log(ciliegi$volume)) - mean(log(ciliegi$diametro)) * beta_hat_ols
+z <- log(ciliegi$volume)
+w <- log(ciliegi$diametro)
+
+beta_hat_ols <- cov(z, log(ciliegi$diametro)) / var(w)
+alpha_hat_ols <- mean(z) - mean(w) * beta_hat_ols
 
 param_hat_ols <- c(exp(alpha_hat_ols), beta_hat_ols)
 
