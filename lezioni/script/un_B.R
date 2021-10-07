1a4b4838f258b3de9d2ae4ebe0b49d4d95bd371d
Tommaso Rigon <tommaso.rigon@gmail.com>
2021-10-07 17:11:16 +0200

    Update un_B.R

--- lezioni/script/un_B.R
+++ lezioni/script/un_B.R
@@ -52,3 +52,69 @@ square_root <- function(x) {
 square_root(-2) # La condizione x < 0 è verificata
 
 square_root(36) # La condizione x < 0 NON è verificata
+
+i <- 5 # Partiamo con i = 25
+
+while (i <= 25) { # Ripete l'operazione fintanto che i non è minore o uguale di 25
+  print(i) # Mostra a schermo il valore di i
+  i <- i + 5 # Incrementa il valore di i
+  
+  # In contesti reali, qui ovviamente ci sono altre operazioni da eseguire
+  # ...
+}
+
+# NON ESEGUIRE IL SEGUENTE CODICE!
+#
+# La condizione i <= 25 è sempre vera perché i non viene aggiornato
+i <- 5
+while (i <= 25) {
+  print(i)
+  
+  # Altre operazioni da eseguire
+  # ...
+}
+
+values <- seq(from = 5, to = 25, by = 5)
+values
+
+for (i in values) {
+  print(i + 2) # Mostra a schermo il valore di i + 2
+  
+  # Altre operazioni da eseguire
+  # ...
+}
+
+distances <- function(x) {
+  n <- length(x) # Ottengo la lunghezza del vettore x
+  D <- matrix(0, nrow = n, ncol = n) # Creazione matrice vuota
+  
+  for (i in 1:n) {
+    for (j in 1:n) {
+      D[i, j] <- (x[i] - x[j])^2
+    }
+  }
+  D # Valore da restituire
+}
+
+x <- c(5, 2, 1, 24) # Esempio per verificare che sia corretto
+distances(x)
+
+for (i in 1:100) {
+  condA <- (i %% 3) == 0 # Il numero è un multiplo di 3?
+  condB <- (i %% 5) == 0 # Il numero è un multiplot di 5?
+  
+  if (condA & condB) {
+    print("fizzbuzz")
+  } else if (condA) {
+    print("fizz")
+  } else if (condB) {
+    print("buzz")
+  } else {
+    print(i)
+  }
+}
+
+library(MASS) # Carica in memoria il pacchetto MASS
+library(knitr) # Carica in memoria il pacchetto knitr
+
+install.packages("knitr") # Installa il pacchetto knitr
