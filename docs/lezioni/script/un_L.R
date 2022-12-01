ee4947c8b7cbfed2e02df9c427879aebdd97a119
Tommaso Rigon <tommaso.rigon@gmail.com>
2022-12-01 15:57:43 +0100

    Updates

--- docs/lezioni/script/un_L.R
+++ docs/lezioni/script/un_L.R
@@ -1,24 +1,48 @@
-rm(list = ls())
-ciliegi <- read.csv("https://tommasorigon.github.io/introR/data/ciliegi.csv", header = TRUE)
+## ------------------------------------------------------------------------------------------------
+ciliegi <- read.table("../dataset/ciliegi.csv", header = TRUE, sep = ",")
+
+
+## ------------------------------------------------------------------------------------------------
+## path <- "https://tommasorigon.github.io/introR/data/ciliegi.csv"
+## ciliegi <- read.table(path, header = TRUE, sep = ",")
+
+
+## ------------------------------------------------------------------------------------------------
 head(ciliegi)
 
+
+## ------------------------------------------------------------------------------------------------
 plot(ciliegi)
 
+
+## ------------------------------------------------------------------------------------------------
 # Funzione di perdita che vogliamo minimizzare
 loss <- function(par, y, x) {
   mean((y - par[1] * x^par[2])^2)
 }
 
+
+## ------------------------------------------------------------------------------------------------
 loss(c(1, 1), ciliegi$volume, ciliegi$diametro)
 
-fit_ls <- nlminb(
-  start = c(1, 1), function(param) loss(param, ciliegi$volume, ciliegi$diametro),
-  lower = c(1e-6, 1e-6)
-)
+
+## ------------------------------------------------------------------------------------------------
+fit_ls <- nlminb(start = c(1, 1), function(param) loss(param, ciliegi$volume, ciliegi$diametro),
+                 lower = c(1e-6, 1e-6))
 fit_ls
 
-param_hat_ls <- fit_ls$par
 
+## ------------------------------------------------------------------------------------------------
+# Salvo i risultati
+param_hat_ls <- fit_ls$par
+param_hat_ls
+
+
+## ------------------------------------------------------------------------------------------------
+fit_ls$objective
+
+
+## ------------------------------------------------------------------------------------------------
 z <- log(ciliegi$volume)
 w <- log(ciliegi$diametro)
 
@@ -28,9 +52,15 @@ alpha_hat_ols <- mean(z) - mean(w) * beta_hat_ols
 # Stima ai minimi quadrati, scala trasformata
 param_hat_ols <- c(exp(alpha_hat_ols), beta_hat_ols)
 param_hat_ols
+
 # Varianza residuale
 loss(param_hat_ols, ciliegi$volume, ciliegi$diametro)
 
+
+## ------------------------------------------------------------------------------------------------
 plot(ciliegi)
-curve(param_hat_ls[1] * x^param_hat_ls[2], add = TRUE, lty = "dashed") # Non-lineari
-curve(param_hat_ols[1] * x^param_hat_ols[2], add = TRUE, lty = "dashed", col = "red") # Modello linearizzato
+curve(param_hat_ls[1] * x^param_hat_ls[2], 
+      add = TRUE, lty = "dashed") # Stime numeriche non-lineari
+curve(param_hat_ols[1] * x^param_hat_ols[2], 
+      add = TRUE, lty = "dashed", col = "red") # Modello linearizzato
+
