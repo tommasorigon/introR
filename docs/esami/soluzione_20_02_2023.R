e7f8e177ebdca34e603a53065c5f6911560ccfe8
Tommaso Rigon <tommaso.rigon@gmail.com>
2023-02-21 12:24:25 +0100

    Updates

--- docs/esami/soluzione_20_02_2023.R
+++ docs/esami/soluzione_20_02_2023.R
@@ -1,25 +1,28 @@
-# Problema 1
+# Problema 1 --------------------------------
 
 # 1.1
+set.seed(123)
 R <- 10^6
 x <- runif(R)
 y <- runif(R)
-mean(exp(sqrt(x * y)))
+mean(exp(sqrt(x * y))) # 1.601412
 
 # Non richiesto. Verifico il risultato tramite integrazione numerica
 library(cubature)
 f <- function(z) exp(sqrt(z[1] * z[2]))
-hcubature(f, lowerLimit = c(0, 0), upperLimit = c(1, 1))
+hcubature(f, lowerLimit = c(0, 0), upperLimit = c(1, 1))$integral # 1.60152
 
 # 1.2
 x <- runif(R, 0, 2)
-g <- function(x) 2 * exp(sqrt(x)) # Attenzione al fatto che bisogna moltiplicare per 2, per tenere conto della costante di normalizzazione di una distribuzione uniforme in (0, 2)
+# Attenzione al fatto che bisogna moltiplicare per 2 la seguente funzione, per tenere conto della costante di normalizzazione di una distribuzione uniforme in (0, 2)
+g <- function(x) 2 * exp(sqrt(x)) 
 est <- mean(g(x))
 std_error <- sd(g(x)) / sqrt(R)
-c(est, std_error)
+c(est, std_error) # 5.406536437 0.001640879
 
 # Non richiesto. Verifico il risultato tramite integrazione numerica:
 integrate(function(x) exp(sqrt(x)), lower = 0, upper = 2)
+# 5.407529 with absolute error < 0.00053
 
 # 1.3
 
@@ -29,7 +32,7 @@ my_integral <- function(f, g, R = 1000) {
   mean(f(x) / g(x))
 }
 
-# Problema 2
+# Problema 2 --------------------------------
 library(MASS)
 data(faithful)
 
@@ -42,9 +45,13 @@ long_waiting <- faithful$waiting[faithful$eruptions > 3]
 short_waiting <- faithful$waiting[faithful$eruptions <= 3]
 
 # 2.3
-plot(ecdf(long_waiting), xlim = c(40, 100)) # Il parametro xlim doveva essere aggiustato
+
+# Quanto segue è la soluzione più elegante, che consente di confrontare le due funzioni di ripartizione
+# Anche due grafici sono stati valutati positivamente (anche se di più difficile lettura)
+plot(ecdf(long_waiting), xlim = c(40, 100)) # Il parametro xlim deve essere manualmente aggiustato, altrimenti il grafico risulta illeggibile
 plot(ecdf(short_waiting), add = TRUE, col = "red")
 
+# Anche calcolare mean e median separatamente era corretto
 summary(long_waiting)
 summary(short_waiting)
 
@@ -76,7 +83,10 @@ qJB <- function(p, n, R = 1000) {
 }
 
 # 2.2
-qJB(p = 0.95, n = 50, R = 10000)
+
+# Queste stime sono molto imprecise anche per valori di R moderatamente elevati
+set.seed(123)
+qJB(p = 0.95, n = 50, R = 10000) # 4.288469 
 
 # Quanto segue non è materiale d'esame, ma potrebbe esservi utile in futuro ------------
 
