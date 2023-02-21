94e5df3b7c60c1754f6b6ea5f4115ded7e281bb5
Tommaso Rigon <tommaso.rigon@gmail.com>
2023-02-21 12:21:29 +0100

    Updates

---
+++ docs/esami/soluzione_20_02_2023.R
@@ -0,0 +1,103 @@
+# Problema 1
+
+# 1.1
+R <- 10^6
+x <- runif(R)
+y <- runif(R)
+mean(exp(sqrt(x * y)))
+
+# Non richiesto. Verifico il risultato tramite integrazione numerica
+library(cubature)
+f <- function(z) exp(sqrt(z[1] * z[2]))
+hcubature(f, lowerLimit = c(0, 0), upperLimit = c(1, 1))
+
+# 1.2
+x <- runif(R, 0, 2)
+g <- function(x) 2 * exp(sqrt(x)) # Attenzione al fatto che bisogna moltiplicare per 2, per tenere conto della costante di normalizzazione di una distribuzione uniforme in (0, 2)
+est <- mean(g(x))
+std_error <- sd(g(x)) / sqrt(R)
+c(est, std_error)
+
+# Non richiesto. Verifico il risultato tramite integrazione numerica:
+integrate(function(x) exp(sqrt(x)), lower = 0, upper = 2)
+
+# 1.3
+
+# Le funzioni f(x) e g(x) devono essere valutate sullo stesso insieme di valori. La soluzione corretta è quindi:
+my_integral <- function(f, g, R = 1000) {
+  x <- rnorm(R)
+  mean(f(x) / g(x))
+}
+
+# Problema 2
+library(MASS)
+data(faithful)
+
+# 2.1
+hist(faithful$waiting)
+plot(faithful$eruptions, faithful$waiting)
+
+# 2.2
+long_waiting <- faithful$waiting[faithful$eruptions > 3]
+short_waiting <- faithful$waiting[faithful$eruptions <= 3]
+
+# 2.3
+plot(ecdf(long_waiting), xlim = c(40, 100)) # Il parametro xlim doveva essere aggiustato
+plot(ecdf(short_waiting), add = TRUE, col = "red")
+
+summary(long_waiting)
+summary(short_waiting)
+
+# 2.4
+asym <- function(x) {
+  Q <- quantile(x, probs = c(0.25, 0.5, 0.75))
+  B <- (Q[1] + Q[3] - 2 * Q[2]) / (Q[3] - Q[1])
+  B
+}
+
+asym(long_waiting)
+asym(short_waiting)
+
+# Problema 3
+
+# 2.1
+JB <- function(x) {
+  n <- length(x)
+  gamma <- mean((x - mean(x))^3) / sd(x)^3
+  kappa <- mean((x - mean(x))^4) / sd(x)^4
+  n / 6 * (gamma^2 + (kappa - 3)^2 / 4)
+}
+
+# Un possibile modo di ragionare era basato sulla funzione replicate (usata nel modo giusto!)
+# Soluzioni corrette basate su "cicli for" sono state valutate positivamente.
+qJB <- function(p, n, R = 1000) {
+  sim <- replicate(R, JB(rnorm(n)))
+  quantile(sim, probs = p)
+}
+
+# 2.2
+qJB(p = 0.95, n = 50, R = 10000)
+
+# Quanto segue non è materiale d'esame, ma potrebbe esservi utile in futuro ------------
+
+# In realtà, il "vero" test JB non utilizza sd bensì la sua versione in cui la somma degli scarti al quadrato viene divisa per n. La funzione JB pertanto si dovrebbe calcola come segue:
+JB <- function(x) {
+  n <- length(x)
+  sigma <- sqrt(mean(x^2) - mean(x)^2)
+  gamma <- mean((x - mean(x))^3) / sigma^3
+  kappa <- mean((x - mean(x))^4) / sigma^4
+  n / 6 * (gamma^2 + (kappa - 3)^2 / 4)
+}
+
+# Il test di Jarque-Bera è implementato in R dentro il pacchetto tseries:
+library(tseries)
+
+x <- rnorm(100)
+JB(x)
+jarque.bera.test(x)
+
+# Se mai vi capitasse di usare questo test in econometria, ricordatevi di questo esercizio e confrontate il valore della funzione qJB...
+qJB(p = 0.95, n = 50, R = 10000)
+
+# ...con l'approssimazione asintotica chi-quadrato, che è pari a
+qchisq(0.95, df = 2)
